432946aab0c66e3b5e7bb730eaf57fe3200088e6
wulei <rabbit586@163.com>
2019-04-12 18:05:47 +0800

    登录流程尝试

--- miniprogram/pages/user/index.js
+++ miniprogram/pages/user/index.js
@@ -38,12 +38,29 @@ Page({
   },
 
   onGetUserInfo: function(e) {
+    let _this = this;
     if (!this.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
+      // 调用云函数
+      wx.cloud.callFunction({
+        name: 'login',
+        data: {},
+        success: res => {
+          console.log('[云函数] [login] user openid: ', res.result.openid)
+          app.globalData.openid = res.result.openid
+          _this.setData({
+            logged: true,
+            avatarUrl: e.detail.userInfo.avatarUrl,
+            userInfo: e.detail.userInfo
+          })
+        },
+        fail: err => {
+          console.error('[云函数] [login] 调用失败', err)
+          wx.navigateTo({
+            url: '../deployFunctions/deployFunctions',
+          })
+        }
       })
+
     }
   },
 
